dd1a7c74c7b34c1d0a2d4951f2fb718f7407308d
Ihtesham Ul Haq <ihteshamulhaq510@gmail.com>
2023-09-16 22:05:30 +0500

    added a keymap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -22,6 +22,7 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "E", "ge", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
