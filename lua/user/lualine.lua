43d544fc5b18829cac2619b425307d1e1b951b6a
Ihtesham Ul Haq <ihteshamulhaq510@gmail.com>
2023-09-22 16:10:49 +0500

    added some configs

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -22,7 +22,7 @@ local diff = {
 	colored = true,
 	update_in_insert = true,
 	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-  cond = hide_in_width
+	cond = hide_in_width,
 }
 
 local mode = {
@@ -49,20 +49,6 @@ local location = {
 	padding = 0,
 }
 
--- cool function for progress
-local progress = function()
-	local current_line = vim.fn.line(".")
-	local total_lines = vim.fn.line("$")
-	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
-end
-
-local spaces = function()
-	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
-end
-
 lualine.setup({
 	options = {
 		icons_enabled = true,
@@ -71,15 +57,20 @@ lualine.setup({
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
 		always_divide_middle = true,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		},
 	},
 	sections = {
-		lualine_a = { branch, diagnostics },
-		lualine_b = { mode },
-		lualine_c = {},
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diff, spaces, "encoding", filetype },
-		lualine_y = { location },
-		lualine_z = { progress },
+		lualine_a = { mode },
+		lualine_b = { branch },
+		lualine_c = { diff },
+		lualine_x = { diagnostics, filetype, "filesize" },
+		-- lualine_x = { diff, spaces, "encoding", filetype },
+		lualine_y = { "progress" },
+		lualine_z = { location },
 	},
 	inactive_sections = {
 		lualine_a = {},
