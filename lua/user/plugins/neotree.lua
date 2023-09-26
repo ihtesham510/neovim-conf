8b93e15e5b70304a1d5bb1c7ec8a37a301bc1959
Ihtesham Ul Haq <ihteshamulhaq510@gmail.com>
2023-09-26 19:04:32 +0500

    added some keymapping in neotree

--- lua/user/plugins/neotree.lua
+++ lua/user/plugins/neotree.lua
@@ -149,7 +149,7 @@ return {
           -- ["s"] = "vsplit_with_window_picker",
           --["t"] = "open_tabnew",
           -- ["<cr>"] = "open_drop",
-          -- ["t"] = "open_tab_drop",
+          ["o"] = "open_tab_drop",
           ["w"] = "open_with_window_picker",
           --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
           ["C"] = "close_node",
@@ -179,6 +179,7 @@ return {
           --}
           ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
           ["q"] = "close_window",
+          ["t"] = "close_window",
           ["R"] = "refresh",
           ["?"] = "show_help",
           ["<"] = "prev_source",
