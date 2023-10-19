ca995f4789dfa5d2d33287d0a3cfcf166199697c
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2023-10-19 17:47:42 +0500

    changed the crates keymappings

--- lua/user/plugins/lsp/rust-ls/crates.lua
+++ lua/user/plugins/lsp/rust-ls/crates.lua
@@ -12,24 +12,24 @@ return {
         },
       },
     })
-    keymap("n", "<leader>Cu", "<cmd>lua require('crates').update_crate()<CR>", { desc = "Update crate" })
-    keymap("n", "<leader>CU", "<cmd>lua require('crates').update_all_crates()<CR>", { desc = "Update all crates" })
-    keymap("n", "<leader>Ci", "<cmd>lua require('crates').show_popup()<CR>", { desc = "Crate Info" })
+    keymap("n", "<leader>lcu", "<cmd>lua require('crates').update_crate()<CR>", { desc = "Update crate" })
+    keymap("n", "<leader>lcU", "<cmd>lua require('crates').update_all_crates()<CR>", { desc = "Update all crates" })
+    keymap("n", "<leader>lci", "<cmd>lua require('crates').show_popup()<CR>", { desc = "Crate Info" })
     keymap(
       "n",
-      "<leader>Cd",
+      "<leader>lcd",
       "<cmd>lua require('crates').show_dependencies_popup()<CR>",
       { desc = "Crate Dependencies" }
     )
     keymap(
       "n",
-      "<leader>Cv",
+      "<leader>lcv",
       "<cmd>lua require('crates').show_versions_popup()<CR><cmd>lua require('crates').show_versions_popup()<CR>",
       { desc = "Crate version" }
     )
     keymap(
       "n",
-      "<leader>Cf",
+      "<leader>lcf",
       "<cmd>lua require('crates').show_features_popup()<CR><cmd>lua require('crates').show_features_popup()<CR>",
       { desc = "Crate features" }
     )
