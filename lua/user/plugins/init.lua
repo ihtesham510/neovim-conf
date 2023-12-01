d9f985ec0402dcc6f96643e503e58113a0d4bd3a
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2023-12-01 21:55:16 +0500

    removed vim tmux nevigator and bg.nvim

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -2,9 +2,4 @@ return {
   "nvim-lua/plenary.nvim",
   "moll/vim-bbye",
   "lewis6991/impatient.nvim",
-  { "typicode/bg.nvim", lazy = false },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
 }
