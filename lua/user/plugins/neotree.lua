b793e19c04a63852d06b5451ad27d96b54e8920b
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2023-12-06 17:56:51 +0500

    removed the lazyloading in neotree

--- lua/user/plugins/neotree.lua
+++ lua/user/plugins/neotree.lua
@@ -3,7 +3,6 @@ return {
   branch = "v3.x",
   cmd = "Neotree" ,
   dependencies = {
-  event = { "BufReadPre", "BufNewFile" },
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
