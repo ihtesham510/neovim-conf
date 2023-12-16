d45c6618f838e3c2988017526d03ab00e1d0cc03
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2023-12-16 22:32:23 +0500

    added formatting settings in formatter.lua

--- lua/plugins/LSP/formatting.lua
+++ lua/plugins/LSP/formatting.lua
@@ -1,29 +1,45 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-    },
-    formatters = {
-      injected = { options = { ignore_errors = true } },
-    },
-    main = "conform",
-  },
-  keys = {
-    {
-      "<leader>W",
-      function()
-        local cf = require("conform")
-        cf.format({ async = false, lsp_fallback = true })
-        vim.cmd([[w!]])
-      end,
-      desc = "Format and save",
-    },
-  },
-  init = function()
-    -- If you want the formatexpr, here is the place to set it
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			["lua"] = { "stylua" },
+			["javascript"] = { "prettier" },
+			["javascriptreact"] = { "prettier" },
+			["typescript"] = { "prettier" },
+			["typescriptreact"] = { "prettier" },
+			["vue"] = { "prettier" },
+			["css"] = { "prettier" },
+			["scss"] = { "prettier" },
+			["less"] = { "prettier" },
+			["html"] = { "prettier" },
+			["json"] = { "prettier" },
+			["jsonc"] = { "prettier" },
+			["yaml"] = { "prettier" },
+			["markdown"] = { "prettier" },
+			["markdown.mdx"] = { "prettier" },
+			["graphql"] = { "prettier" },
+			["handlebars"] = { "prettier" },
+		},
+		formatters = {
+			injected = { options = { ignore_errors = true } },
+		},
+		main = "conform",
+	},
+	keys = {
+		{
+			"<leader>w",
+			function()
+				local cf = require("conform")
+				cf.format({ async = false, lsp_fallback = true })
+				vim.cmd([[w!]])
+			end,
+			desc = "Format and save",
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 }
