6c94472572f049fa330f3de3fb4ad050e51ff12a
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2023-12-17 15:51:24 +0500

    formatted the file

--- lua/plugins/LSP/MarkdownPreview.lua
+++ lua/plugins/LSP/MarkdownPreview.lua
@@ -1,18 +1,18 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    keys = {
-      {
-        "<leader>P",
-        function()
-          vim.cmd([[MarkdownPreviewToggle]])
-        end,
-        desc = "Peek (Markdown Preview)",
-      },
-    },
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  }
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && yarn install",
+	keys = {
+		{
+			"<leader>P",
+			function()
+				vim.cmd([[MarkdownPreviewToggle]])
+			end,
+			desc = "Peek (Markdown Preview)",
+		},
+	},
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+	end,
+	ft = { "markdown" },
+}
