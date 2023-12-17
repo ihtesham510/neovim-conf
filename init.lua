f61dc23d47e60003af45a71d3881fe70e25a8476
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2023-12-17 15:51:15 +0500

    changed the import style

--- init.lua
+++ init.lua
@@ -14,4 +14,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
-require("lazy").setup({ { import = "plugins/core" }, { import = "plugins/LSP" } })
+require("lazy").setup({ { import = "plugins.core" }, { import = "plugins.LSP" } })
