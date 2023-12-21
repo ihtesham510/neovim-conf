208705e06300c49f86b852a197f1b2ac32101d51
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2023-12-21 12:30:04 +0500

    added config for treesitter and comment.nvim

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -1,64 +1,76 @@
 return {
 	{
+
 		"nvim-treesitter/nvim-treesitter",
-		version = false, -- last release is way too old and doesn't work on Windows
+		event = { "BufReadPre", "BufNewFile" },
 		build = ":TSUpdate",
-		event = { "BufReadPost", "BufNewFile" },
-		opts = {
-			ensure_installed = {
-				"bash",
-				"html",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"regex",
-				"vim",
-				"yaml",
-				"scss",
-				"graphql",
-			},
-			highlight = { enable = true },
-			indent = { enable = true, disable = { "yaml", "python", "html" } },
-			context_commentstring = { enable = true },
-			rainbow = {
-				enable = true,
-				query = "rainbow-parens",
-				disable = { "jsx", "html" },
-			},
+		dependencies = {
+			"windwp/nvim-ts-autotag",
 		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				-- A list of parser names, or "all" (the five listed parsers should always be installed)
+				ensure_installed = { "c", "lua", "cpp", "rust" },
+
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = false,
+
+				-- for autotag
+				autotag = { enable = true },
+
+				-- Automatically install missing parsers when entering buffer
+				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+				auto_install = true,
+
+				-- List of parsers to ignore installing (or "all")
+				-- ignore_install = { "javascript" },
+
+				---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+				-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+				highlight = {
+					enable = true,
+
+					-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+					-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+					-- the name of the parser)
+					-- list of language that will be disabled
+					-- disable = { "c", "rust" },
+					-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+					disable = function(lang, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end,
+
+					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+					-- Using this option may slow down your editor, and you may see some duplicate highlights.
+					-- Instead of true it can also be a list of languages
+					additional_vim_regex_highlighting = false,
+				},
+			})
 		end,
 	},
-
 	{
-		"HiPhish/rainbow-delimiters.nvim",
-		init = function()
-			local rainbow_delimiters = require("rainbow-delimiters")
+		"numToStr/Comment.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		config = function()
+			-- import comment plugin safely
+			local comment = require("Comment")
 
-			vim.g.rainbow_delimiters = {
-				strategy = {
-					[""] = rainbow_delimiters.strategy["global"],
-					vim = rainbow_delimiters.strategy["local"],
-				},
-				query = {
-					[""] = "rainbow-delimiters",
-					lua = "rainbow-blocks",
-					tsx = "rainbow-parens",
-					javascript = "rainbow-delimiters-react",
-				},
-				highlight = {
-					"RainbowDelimiterRed",
-					"RainbowDelimiterYellow",
-					"RainbowDelimiterBlue",
-					"RainbowDelimiterOrange",
-					"RainbowDelimiterGreen",
-					"RainbowDelimiterViolet",
-					"RainbowDelimiterCyan",
-				},
-			}
+			local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+			-- enable comment
+			comment.setup({
+				-- for commenting tsx and jsx files
+				pre_hook = ts_context_commentstring.create_pre_hook(),
+			})
 		end,
 	},
 }
