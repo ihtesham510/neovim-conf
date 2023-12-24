ed2a5c4b69ec0284bb77857e93baeb6f6ae8c6a2
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2023-12-24 22:20:31 +0500

    changed the inc-rename keymapping

--- lua/plugins/core/editor.lua
+++ lua/plugins/core/editor.lua
@@ -3,7 +3,7 @@ return {
 		"smjonas/inc-rename.nvim",
 		config = function()
 			require("inc_rename").setup({})
-			vim.keymap.set("n", "<leader>rn", function()
+			vim.keymap.set("n", "R", function()
 				return ":IncRename " .. vim.fn.expand("<cword>")
 			end, { expr = true })
 		end,
@@ -246,5 +246,42 @@ return {
 				"hbs",
 			},
 		},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			icons = {
+				breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+				separator = "➜", -- symbol used between a key and it's label
+				group = "+", -- symbol prepended to a group
+			},
+			window = {
+				border = "double", -- none, single, double, shadow
+				position = "bottom", -- bottom, top
+				margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
+				padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
+				winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+				zindex = 1000, -- positive value to position WhichKey above other floating windows.
+			},
+			layout = {
+				height = { min = 4, max = 25 }, -- min and max height of the columns
+				width = { min = 20, max = 50 }, -- min and max width of the columns
+				spacing = 3, -- spacing between columns
+				align = "left", -- align columns left, center or right
+			},
+		},
+		config = function()
+			local wk = require("which-key")
+			wk.register({
+				l = {
+					name = "lsp", -- optional group name
+				},
+			}, { prefix = "<leader>" })
+		end,
 	},
 }
