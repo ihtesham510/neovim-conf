b434826ead9847b736a92aafa81ece1c77477c35
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-01-27 19:37:20 +0500

    changed colorscheme to solarised dark

--- lua/plugins/core/colorscheme.lua
+++ lua/plugins/core/colorscheme.lua
@@ -2,8 +2,13 @@ return {
 	{
 		"Mofiqul/vscode.nvim",
 		lazy = false,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme vscode]])
+			vim.cmd([[colorscheme tokyonight-storm]])
 			vim.o.background = "dark"
 		end,
 	},
