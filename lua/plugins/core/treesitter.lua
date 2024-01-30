90aba2daf8d93c616886e84bf074e2ba32e2ec40
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-01-30 20:55:34 +0500

    added markdown_inline in treesitter

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -10,7 +10,7 @@ return {
 		config = function()
 			require("nvim-treesitter.configs").setup({
 				-- A list of parser names, or "all" (the five listed parsers should always be installed)
-				ensure_installed = { "c", "lua", "cpp", "rust" },
+				ensure_installed = { "c", "lua", "cpp", "rust", "markdown_inline" },
 
 				-- Install parsers synchronously (only applied to `ensure_installed`)
 				sync_install = false,
