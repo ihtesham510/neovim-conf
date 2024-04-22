0991d560405a96f3aaf2c17daf9d0a17fb68a341
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-04-22 11:32:05 +0500

    changed gitsings

--- lua/plugins/core/gitsigns.lua
+++ lua/plugins/core/gitsigns.lua
@@ -1,27 +1,35 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPost", "BufNewFile" },
-  keys = {
-    {"gj","<cmd>lua require 'gitsigns'.next_hunk()<cr>",desc = "Git Next Hunk"},
-    {"gk","<cmd>lua require 'gitsigns'.prev_hunk()<cr>",desc = "Git Previous Hunk"},
-    {"gB","<cmd>lua require 'gitsigns'.blame_line()<cr>",desc = "Git Blame"},
-    {"gr","<cmd>lua require 'gitsigns'.reset_hunk()<cr>",desc = "Git Reset Hunk"},
-    {"gR","<cmd>lua require 'gitsigns'.reset_buffer()<cr>",desc = "Git Reset buffer"},
-    {"gs","<cmd>lua require 'gitsigns'.stage_hunk()<cr>",desc = "Git stage hunk"},
-    {"gu","<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",desc = "git undostage hunk"},
-    {"gS","<cmd>Telescope git_status<cr>",desc = "Git Status"},
-    { "gb", "<cmd>Telescope git_branches<cr>", desc = "Checkout branch" },
-    { "gc", "<cmd>Telescope git_commits<cr>", desc = "Checkout commit" },
-  },
+	keys = {
+		{ "gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>", desc = "Git Next Hunk" },
+		{ "gk", "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", desc = "Git Previous Hunk" },
+		{ "gB", "<cmd>lua require 'gitsigns'.blame_line()<cr>", desc = "Git Blame" },
+		{ "gr", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", desc = "Git Reset Hunk" },
+		{ "gR", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", desc = "Git Reset buffer" },
+		{ "gs", "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", desc = "Git stage hunk" },
+		{ "gu", "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", desc = "git undostage hunk" },
+		{ "gS", "<cmd>Telescope git_status<cr>", desc = "Git Status" },
+		{ "gb", "<cmd>Telescope git_branches<cr>", desc = "Checkout branch" },
+		{ "gc", "<cmd>Telescope git_commits<cr>", desc = "Checkout commit" },
+	},
 	config = function()
 		require("gitsigns").setup({
+			-- signs = {
+			-- 	add = { text = "" },
+			-- 	change = { text = "" },
+			-- 	delete = { text = "" },
+			-- 	topdelete = { text = "󱅁" },
+			-- 	changedelete = { text = "󰍷" },
+			-- 	untracked = { text = "" },
+			-- },
 			signs = {
-				add = { text = "" },
-				change = { text = "" },
-				delete = { text = "" },
-				topdelete = { text = "󱅁" },
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
 				changedelete = { text = "󰍷" },
-				untracked = { text = "" },
+				untracked = { text = "┆" },
 			},
 			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
