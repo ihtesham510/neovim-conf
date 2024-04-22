388962eb197e366f4cfc9947f874d889adfe2a04
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-04-22 11:27:55 +0500

    changed lsp signs

--- lua/plugins/LSP/lsp_config.lua
+++ lua/plugins/LSP/lsp_config.lua
@@ -84,7 +84,7 @@ return {
 				cmd = { "dart", "language-server", "--protocol=lsp" },
 			})
 
-			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+			local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 			for type, icon in pairs(signs) do
 				local hl = "DiagnosticSign" .. type
 				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
