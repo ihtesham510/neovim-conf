bb9b7251e32a16a6ef8098ce785f00681ac99056
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-04-22 11:25:59 +0500

    removed nonols and added mason tool installer

--- lua/plugins/LSP/lsp.lua
+++ lua/plugins/LSP/lsp.lua
@@ -1,51 +1,60 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		event = { "BufRead", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			"jayp0521/mason-null-ls.nvim",
-		},
-		config = function()
-			local mason_lspconfig = require("mason-lspconfig")
-			local mason = require("mason")
-			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-			end
-
-			mason.setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-			mason_lspconfig.setup({
-				ensure_installed = {
-					"lua_ls",
-					"rust_analyzer",
-					"tsserver",
-					"jsonls",
-					"cssls",
-					"tailwindcss",
-					"taplo",
-					"svelte",
-					"clangd",
-					"prismals",
-					"graphql",
-					"emmet_ls",
-					"pyright",
-					"tailwindcss",
-				},
-			})
-			local keymap = vim.keymap.set
-			local opts = { noremap = true, silent = true }
-			opts.desc = "Open Mason"
-			keymap("n", "<leader>m", "<cmd>Mason<CR>", opts) -- show definition, references
-		end,
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"tsserver",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"clangd",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+				"taplo",
+				"clangd",
+				"jsonls",
+				"rust_analyzer",
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint",
+				"eslint_d",
+			},
+		})
+		local keymap = vim.keymap.set
+		local opts = { noremap = true, silent = true }
+		opts.desc = "Open Mason"
+		keymap("n", "<leader>m", "<cmd>Mason<CR>", opts) -- show definition, references
+	end,
 }
