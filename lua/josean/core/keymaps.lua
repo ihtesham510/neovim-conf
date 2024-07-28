82d78d624c1743d7ceb4484f222762dc77ff2756
ihtesham <ihteshamulhaq510@gmail.com>
2024-07-28 21:12:51 +0500

    added keymaps

--- lua/josean/core/keymaps.lua
+++ lua/josean/core/keymaps.lua
@@ -1,23 +1,29 @@
 vim.g.mapleader = " "
 
-local keymap = vim.keymap -- for conciseness
+local keymap = vim.keymap.set -- for conciseness
+local opts = { noremap = true, silent = true }
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap("i", "kj", "<ESC>", { desc = "Exit insert mode with kj" })
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
+keymap("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap("v", "q", "<ESC>", { desc = "Exit Visual Mode" })
+-- save and exit
+keymap("n", "<leader>w", ":w<CR>", { desc = "Save File" })
+keymap("n", "<leader>q", ":qa<CR>", { desc = "Quit" })
+-- Move text up and down
+keymap("x", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("x", "K", ":m '<-2<CR>gv=gv", opts)
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+keymap("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap("n", "<leader>sl", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap("n", "<leader>sj", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+-- tabs
+keymap("n", "L", "<cmd>bnext<CR>", { desc = "Next tab" }) -- open new tab
+keymap("n", "H", "<cmd>bprevious<CR>", { desc = "Previous tab" }) -- close current tab
+keymap("n", "<leader>c", "<cmd>bd!<CR>", { desc = "Close tab" }) --  move current buffer to new tab
