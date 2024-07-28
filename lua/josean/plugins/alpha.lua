d08f96da89a6d0e3ae70a028a420dee0a3d0b05c
ihtesham <ihteshamulhaq510@gmail.com>
2024-07-28 21:18:11 +0500

    modified alpha

--- lua/josean/plugins/alpha.lua
+++ lua/josean/plugins/alpha.lua
@@ -1,11 +1,14 @@
 return {
   "goolord/alpha-nvim",
   event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
+    local keymap = vim.keymap.set
 
     -- Set header
+    --
     dashboard.section.header.val = {
       "                                                     ",
       "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
@@ -16,16 +19,18 @@ return {
       "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
       "                                                     ",
     }
-
     -- Set menu
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+      dashboard.button("f", "󰱼  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+      dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+      dashboard.button("F", "  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("l", "󰒲  Open Lazy", ":Lazy <CR>"),
+      dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
+      dashboard.button("q", "󰗼  Quit Neovim", ":qa<CR>"),
     }
+    keymap("n", "<leader>a", "<cmd>Alpha<cr>", { desc = "Open Alpha" })
 
     -- Send config to alpha
     alpha.setup(dashboard.opts)
