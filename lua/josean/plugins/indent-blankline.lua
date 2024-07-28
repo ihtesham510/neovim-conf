980965b635f9d371afc3b89061ee25e15167d19e
ihtesham <ihteshamulhaq510@gmail.com>
2024-07-28 20:55:11 +0500

    modified indent blankline

--- lua/josean/plugins/indent-blankline.lua
+++ lua/josean/plugins/indent-blankline.lua
@@ -1,8 +1,21 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  dependencies = {
+    {
+      "echasnovski/mini.indentscope",
+      version = false, -- wait till new 0.7.0 release to put it back on semver
+      event = "VeryLazy",
+      opts = {
+        -- symbol = "",
+        symbol = "│",
+        options = { try_as_border = true },
+      },
+    },
+  },
   event = { "BufReadPre", "BufNewFile" },
   main = "ibl",
   opts = {
     indent = { char = "┊" },
+    scope = { enabled = false },
   },
 }
