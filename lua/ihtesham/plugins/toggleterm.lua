5f149e746a349ad4a8b9d1ba8ee6ee54cf89aef3
ihtesham <ihteshamulhaq510@gmail.com>
2024-07-28 21:52:28 +0500

    added toggleterm

---
+++ lua/ihtesham/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+  "akinsho/toggleterm.nvim",
+  opts = {
+    open_mapping = [[<A-f>]],
+    persist_size = true,
+    direction = "float",
+    float_opts = {
+      border = "curved",
+      winblend = 0,
+      highlights = {
+        border = "Normal",
+        background = "Normal",
+      },
+    },
+  },
+}
