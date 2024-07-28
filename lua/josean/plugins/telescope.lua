7a2084682c63a140356318c394e6eea7f87436e0
ihtesham <ihteshamulhaq510@gmail.com>
2024-07-28 20:34:36 +0500

    modified telescope.lua

--- lua/josean/plugins/telescope.lua
+++ lua/josean/plugins/telescope.lua
@@ -10,41 +10,57 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
-    local transform_mod = require("telescope.actions.mt").transform_mod
-
-    local trouble = require("trouble")
-    local trouble_telescope = require("trouble.sources.telescope")
-
-    -- or create your custom action
-    local custom_actions = transform_mod({
-      open_trouble_qflist = function(prompt_bufnr)
-        trouble.toggle("quickfix")
-      end,
-    })
-
     telescope.setup({
       defaults = {
-        path_display = { "smart" },
+        color_devicons = true,
+        set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+        initial_mode = "normal",
+        file_ignore_patterns = { "node%_modules/.*", "target/*", ".git/*" },
+        prompt_prefix = "     ",
+        selection_caret = " ❯ ",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+            results_width = 0.8,
+          },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 60,
+        },
+        path_display = { "truncate" },
         mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.open,
+          n = {
+            ["<esc>"] = actions.close,
+            ["q"] = actions.close,
+            ["<CR>"] = actions.select_default,
+            ["l"] = actions.select_default,
+            ["h"] = actions.select_horizontal,
+            ["s"] = actions.select_vertical,
           },
         },
       },
     })
-
     telescope.load_extension("fzf")
     telescope.load_extension("projects")
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
+    -- files
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>sr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "sr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    -- string
+    keymap.set("n", "F", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+    keymap.set("n", "fs", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    -- todo
+    keymap.set("n", "ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
 }
