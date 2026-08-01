47827850d0169726de1462961941cd1c6a5d9484
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2026-08-01 22:09:49 +0500

    added neccary plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,1 +1,65 @@
-return {}
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        progress = { enabled = true }, -- ✅ show LSP progress
+        hover = { enabled = true }, -- ❌ disable LSP hover
+        signature = { enabled = true }, -- ❌ disable LSP signature help
+        message = { enabled = false }, -- ❌ disable LSP messages
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = false,
+        inc_rename = true,
+        lsp_doc_border = true,
+      },
+      -- disable all other routes/views
+      messages = { enabled = false },
+      cmdline = { enabled = true },
+      popupmenu = { enabled = true },
+      notify = { enabled = false },
+    },
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>gl", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+    },
+  },
+  { -- Collection of various small independent plugins/modules
+    "echasnovski/mini.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    opts = {
+      open_mapping = [[<A-f>]],
+      persist_size = true,
+      direction = "float",
+      float_opts = {
+        border = "curved",
+        winblend = 0,
+        highlights = {
+          border = "Normal",
+          background = "Normal",
+        },
+      },
+    },
+  },
+}
